ae1378518cfde2f42ab9d74cd2599a3d3cbd44b8
Sergey Khadanovich <khadanovich.sergey@gmail.com>
2021-11-17 21:20:52 +0300

    Feature/s inspection (#6)
    * Add interpolated string validity inspection.

---
+++ src/test/java/dev/khbd/interp4j/intellij/BaseIntellijTest.java
@@ -0,0 +1,95 @@
+package dev.khbd.interp4j.intellij;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.LanguageLevelProjectExtension;
+import com.intellij.pom.java.LanguageLevel;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
+import com.intellij.testFramework.fixtures.IdeaTestExecutionPolicy;
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
+import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture;
+import com.intellij.testFramework.fixtures.JavaTestFixtureFactory;
+import com.intellij.testFramework.fixtures.TempDirTestFixture;
+import com.intellij.testFramework.fixtures.TestFixtureBuilder;
+import com.intellij.testFramework.fixtures.impl.LightTempDirTestFixtureImpl;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Sergei_Khadanovich
+ */
+public abstract class BaseIntellijTest {
+
+    protected JavaCodeInsightTestFixture fixture;
+
+    @BeforeMethod
+    public void beforeMethod() throws Exception {
+        IdeaTestFixtureFactory factory = IdeaTestFixtureFactory.getFixtureFactory();
+        TestFixtureBuilder<IdeaProjectTestFixture> fixtureBuilder =
+                factory.createLightFixtureBuilder(getProjectDescriptor());
+
+        IdeaProjectTestFixture projectFixture = fixtureBuilder.getFixture();
+
+        fixture = JavaTestFixtureFactory.getFixtureFactory()
+                .createCodeInsightFixture(projectFixture, getTempDirFixture());
+
+        fixture.setTestDataPath(getTestDataPath());
+        fixture.setUp();
+
+        LanguageLevelProjectExtension.getInstance(getProject())
+                .setLanguageLevel(getLanguageLevel());
+
+        LensTestUtil.loadLibrary(fixture.getProjectDisposable(), fixture.getModule(), "interp4j-core", "interp4j-core.jar");
+        LensTestUtil.loadLibrary(fixture.getProjectDisposable(), fixture.getModule(), "interp4j-processor", "interp4j-processor.jar");
+    }
+
+    @AfterMethod
+    public void afterMethod() throws Exception {
+        try {
+            fixture.tearDown();
+        } finally {
+            fixture = null;
+        }
+    }
+
+    protected String getTestDataPath() {
+        return "src/test/resources/testData";
+    }
+
+    protected LanguageLevel getLanguageLevel() {
+        return LanguageLevel.JDK_11;
+    }
+
+    protected LightProjectDescriptor getProjectDescriptor() {
+        return LensTestUtil.createProjectDescriptor(getLanguageLevel());
+    }
+
+    protected Project getProject() {
+        return fixture.getProject();
+    }
+
+    protected TempDirTestFixture getTempDirFixture() {
+        IdeaTestExecutionPolicy policy = IdeaTestExecutionPolicy.current();
+        return policy != null
+                ? policy.createTempDirTestFixture()
+                : new LightTempDirTestFixtureImpl(true);
+    }
+
+    protected <T> T read(Callable<T> callable) throws Exception {
+        CompletableFuture<T> future = new CompletableFuture<>();
+        ApplicationManager.getApplication().invokeLater(() -> {
+            try {
+                T value = callable.call();
+                future.complete(value);
+            } catch (Exception ex) {
+                future.completeExceptionally(ex);
+            }
+        });
+        return future.get(1L, TimeUnit.SECONDS);
+    }
+}
---
+++ src/test/resources/testData/inspection/wrong_string/Main.java
@@ -0,0 +1,16 @@
+package inspection.wrong_string;
+
+import static dev.khbd.interp4j.core.Interpolations.s;
+
+public class Main {
+
+    static String TEMPLATE = "Hello ${name}";
+
+    public static void main(String[] args) {
+        String name = "Alex";
+
+        System.out.println(s(<error descr="Only string literal value can be used in this place">null</error>));
+        System.out.println(s(<error descr="Only string literal value can be used in this place">TEMPLATE</error>));
+        System.out.println(s(<error descr="Only string literal value can be used in this place">"Hello" + " ${name}"</error>));
+    }
+}
