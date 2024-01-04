0fca119fa74028d2d9d67f7ac6e95d876745f3ce
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-01-04 18:28:42 +0300

    Fix implicit usage provider for method arguments and static fields.

--- src/main/java/dev/khbd/interp4j/intellij/usage/InStringLiteralsImplicitUsageProvider.java
+++ src/main/java/dev/khbd/interp4j/intellij/usage/InStringLiteralsImplicitUsageProvider.java
@@ -2,7 +2,9 @@ package dev.khbd.interp4j.intellij.usage;
 
 import com.intellij.codeInsight.daemon.ImplicitUsageProvider;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiLocalVariable;
+import com.intellij.psi.PsiParameter;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.searches.ReferencesSearch;
@@ -12,19 +14,18 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author Sergei_Khadanovich
  */
-public class LocalVariableInStringLiteralsImplicitUsageProvider implements ImplicitUsageProvider {
+public class InStringLiteralsImplicitUsageProvider implements ImplicitUsageProvider {
 
     @Override
     public boolean isImplicitUsage(@NotNull PsiElement element) {
-        if (!(element instanceof PsiLocalVariable)) {
-            return false;
+        if (element instanceof PsiLocalVariable
+                || element instanceof PsiParameter
+                || element instanceof PsiField) {
+            SearchScope scope = element.getUseScope();
+            Query<PsiReference> usageQuery = ReferencesSearch.search(element, scope);
+            return usageQuery.findFirst() != null;
         }
-
-        PsiLocalVariable local = (PsiLocalVariable) element;
-        SearchScope scope = local.getUseScope();
-
-        Query<PsiReference> usageQuery = ReferencesSearch.search(local, scope);
-        return usageQuery.findFirst() != null;
+        return false;
     }
 
     @Override
