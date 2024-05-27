d6dcee2bd8fd813a34b258ccfcedc27fc6e97a51
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-05-27 16:11:45 +0300

    Add `fmt` grammar.

---
+++ src/main/java/dev/khbd/interp4j/intellij/common/grammar/fmt/FormatExpression.java
@@ -0,0 +1,45 @@
+package dev.khbd.interp4j.intellij.common.grammar.fmt;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Format expression model.
+ *
+ * @author Sergei Khadanovich
+ */
+@ToString
+@EqualsAndHashCode
+public class FormatExpression {
+
+    private final List<FormatExpressionPart> parts;
+
+    public FormatExpression(List<FormatExpressionPart> parts) {
+        this.parts = new ArrayList<>(parts);
+    }
+
+    /**
+     * Visit fmt expression.
+     *
+     * @param visitor visitor
+     */
+    public void visit(FormatExpressionVisitor visitor) {
+        visitor.start();
+        for (FormatExpressionPart part : parts) {
+            part.visit(visitor);
+        }
+        visitor.finish();
+    }
+
+    /**
+     * Create empty builder.
+     *
+     * @return builder
+     */
+    public static FormatExpressionBuilder builder() {
+        return new FormatExpressionBuilder();
+    }
+}
---
+++ src/main/java/dev/khbd/interp4j/intellij/common/grammar/fmt/ImplicitIndex.java
@@ -0,0 +1,11 @@
+package dev.khbd.interp4j.intellij.common.grammar.fmt;
+
+/**
+ * Implicit index.
+ *
+ * @author Sergei_Khadanovich
+ */
+public enum ImplicitIndex implements Index {
+
+    INSTANCE
+}
